17406ab5ffd8e873f65b5879761b1639b992e628
dextermoran <dextermoran@gmail.com>
2016-03-15 02:42:22 +0000

    refactor and add reflection

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,6 +1,6 @@
 # Your Names
-# 1)
-# 2)
+# 1)Dexter Moran
+# 2) Lisa Bush
 
 # We spent [#] hours on this challenge.
 
@@ -8,27 +8,34 @@
 
 def serving_size_calc(item_to_make, num_of_ingredients)
   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
-
+  
+  
+#refactor error handling to be more clearer and shorter 17 to 25
   library.each do |food|
-    if library[food] != library[item_to_make]
-      error_counter += -1
+    if ! library.has_key?(item_to_make)
+      p "TEST"
+      raise ArgumentError.new("#{item_to_make} is not a valid input !!")
     end
   end
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
+
+
+#replace case satement with an if statement 32 to 37
 
   serving_size = library.values_at(item_to_make)[0]
   remaining_ingredients = num_of_ingredients % serving_size
 
-  case remaining_ingredients
-  when 0
+  if remaining_ingredients == 0
+        p "test if statement"
+
     return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
   else
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+        p "test else statement"
+    
+
+    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. You should make #{remaining_ingredients} cookies!"
   end
+
 end
 
 p serving_size_calc("pie", 7)
@@ -39,4 +46,27 @@ p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
+
 #  Reflection
+
+=begin
+
+What did you learn about making code readable by working on this challenge?
+
+  We learned what it's like to recieve bad code written by something else and how little fun that is. 
+  This really drove home why we care so much about about the legibility of our code for me. I don't like
+  being in that situation and I'm sure no one else does either. 
+
+Did you learn any new methods? What did you learn about them?
+
+    We used the .has_key? method. This is a method to check if a has has a specific key. 
+    
+What did you learn about accessing data in hashes? 
+
+  We learned a new method mentioned above. We also learned about how .each returns hash keys and values. 
+
+What concepts were solidified when working through this challenge?
+
+  Code legibility, iterating through hashes, and the use of p to test loops are all really important and we had valuable practice with implementing all of these.
+
+=end
