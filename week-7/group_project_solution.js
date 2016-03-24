9cc9cd20ff5fc855ac8df557a2a1601f4cc672af
dextermoran <dextermoran@gmail.com>
2016-03-24 20:29:02 +0000

    add 7.8

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,43 @@
+
+/*
+
+
+PSEUDOCODE 
+
+***Task 1***
+
+  Define a method named sum 
+  Input: an Array of Integers
+  Output: The sum of every number in the array. 
+  
+
+  4. Return total. 
+  
+  
+***Task 2*** 
+  
+  Define a function called mean
+  Input: An Array of Integers
+  Output: The mean (average) of the numbers in the array 
+  
+  1. Define a variable called total and set it equal to zero
+  2. Iterate through the Array that was recieved as an argument. 
+  3. At each index of the array add the value of the current index to total
+  4. Define a new variable called mean and set it equal to total divided by the length of the array. 
+  
+  
+***Task 3*** 
+
+  Define a function called median 
+  Input: an Array of unsorted Integers
+  Output: The median number of the array
+  
+  1. Sort the array passed as an argument so the numbers are ordered smallest to largest (look in the Array docs, there is an easy way to do this.)
+  2. Declare a variable called listMiddle and set it equal to the length of the array divided by two
+  3. Check if the the length of the array is evenly divisible by two 
+  4. If it is (the length of the array is even) then return the value at the index of listMiddle and listMiddle + 1
+  5. If it is not (the length of the array is odd) then return the value at the index listMiddle 
+
+
+*/
+
