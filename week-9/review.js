deb0aeb1f16a7cbd7da6b10c7a897ce2ab4f4b0b
dextermoran <dextermoran@gmail.com>
2016-04-09 15:18:53 +0000

    add review.js

--- week-9/review.js
+++ week-9/review.js
@@ -26,7 +26,7 @@ var guessingGame = {
 
 };
 
-console.log(guessingGame.constructor(12));
+console.log(guessingGame.constructor(12));  
 console.log(guessingGame.guess(9));
 console.log(guessingGame.guess(15));
 console.log(guessingGame.guess(12));
@@ -35,8 +35,77 @@ console.log(guessingGame.solved());
 /* 
 
 What concepts did you solidify in working on this challenge? 
+
+  I got some practice translating between ruby an javascript
+
 What was the most difficult part of this challenge?
+
+  This was my first time assigning functions to an object. 
+
 Did you solve the problem in a new way this time?
-Was your pseudocode different from the Ruby version? What was the same and what was different?
+
+  I did! i used an object instead of a class. I'm not positive this was the best solution but it worked. 
+
+
+*/ 
+
+
+/* 
+Pseudocode:
+
+create a list object
+define a method to add an item
+define a method to remove an item 
+define a method to adjust quantity
+print the list (nicely)
+
+*/ 
+
+var listyList = {
+  constructor: function() {
+    this.addItem = function(item, quantity){
+      listyList[item] = quantity;
+    };
+    
+    this.removeItem = function(item){
+      delete listyList[item];
+    };
+    
+    this.quantity = function(item, newQuantity){
+      listyList[item] = newQuantity;
+    };
+    
+    this.print = function(){
+      console.log(listyList);
+    };
+  }
+};
+
+listyList.constructor();
+//console.log(listyList);
+listyList.addItem("cheese", 10);
+listyList.addItem("meatballs", 2);
+listyList.addItem("pasta", 200)
+//console.log(listyList);
+listyList.removeItem("pasta"); //gluten free
+listyList.quantity("cheese", 15);
+
+console.log(listyList);
+
+
+
+/*
+
+What concepts did you solidify in working on this challenge? (reviewing the passing of information, objects, constructors, etc.)
+
+  this was a decent review of javascript objects and constructors
+
+What was the most difficult part of this challenge?
+
+  It wasn't too challenging really. It was a good review. 
+
+Did an array or object make more sense to use and why?
+
+  An object, because we want our list to have our items as well as their associated quantity. 
 
 */ 
